99e0dd8a6dba983b0e7e6b8449dcb39a70671ac7
josiahgoosen <54074270+josiahgoosen@users.noreply.github.com>
2020-05-19 14:30:22 -0600

    Update data.mysql.sql
    created two tables

--- Squadron Paperwork Tracker/data.mysql.sql
+++ Squadron Paperwork Tracker/data.mysql.sql
@@ -2,3 +2,21 @@ DROP DATABASE IF EXISTS data;
 
 CREATE DATABASE data;
 USE data;
+
+CREATE TABLE form10 (
+  cadetName CHARACTER VARYING(30) NOT NULL,
+  type BOOLEAN, -- 0 for bad, 1 for good
+  
+  PRIMARY KEY(cadetName)
+);
+  
+
+CREATE TABLE cadet (
+	cadetName CHARACTER VARYING(30) NOT NULL,
+	classYear INTEGER(4) NOT NULL,
+	squad INTEGER(2) NOT NULL,
+	form10 BOOLEAN,
+  
+	PRIMARY KEY (cadetName),
+  FOREIGN KEY (form10) REFERENCES form10 (cadetName)
+);
