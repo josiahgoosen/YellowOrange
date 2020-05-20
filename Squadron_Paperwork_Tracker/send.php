111b6545fd3286097bbdf5b6275b827c5ce1ca59
C0MR4D3-GR4D <c21cameron.conrad@usafa.edu>
2020-05-20 04:01:46 -0700

    The search function does not print database files

--- Squadron_Paperwork_Tracker/send.php
+++ Squadron_Paperwork_Tracker/send.php
@@ -1,13 +1,25 @@
 <?php  
-       $servername = "ubuntu";  
+       $servername = "localhost";  
        $username = "student";  
        $password = "CompSci364";
        $database = "data";
-       $link = new mysqli ($servername , $username , $password, 
+       $link = new mysqli ($servername, $username , $password, 
        							$database);  
 
 		if(!$link){
     		die("ERROR: Could not connect. " . mysqli_connect_error());
 		}
-       	echo 'Successful Connection';
+       	
+       	// Escape user inputs for security
+	$squad = mysqli_real_escape_string($link, $_REQUEST['squad']);
+	$cadetName = mysqli_real_escape_string($link, $_REQUEST['cadetName']);
+	$classYear = mysqli_real_escape_string($link, $_REQUEST['classYear']);
+	$form10 = mysqli_real_escape_string($link, $_REQUEST['form10']);
+
+	$sql = "INSERT INTO cadet (squad, cadetName, classYear, form10) VALUES ('$squad', 				'$cadetName', '$classYear', '$form10')";
+	if(mysqli_query($link, $sql)){
+	    echo "Record added successfully.";
+	} else{
+	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+	}
 ?>  
