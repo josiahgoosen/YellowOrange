9726b82b708512ce5981241db2ed91fd4eddb423
C0MR4D3-GR4D <c21cameron.conrad@usafa.edu>
2020-05-20 14:52:08 -0700

    all that is needed is format and prepared statements

--- Squadron_Paperwork_Tracker/search.php
+++ Squadron_Paperwork_Tracker/search.php
@@ -1,3 +1,25 @@
+<html>
+<head>
+
+<style>
+
+table{
+
+border-style:solid;
+
+border-width:2px;
+
+border-color:red;
+
+}
+
+</style>
+
+</head>
+
+<body bgcolor="#EEFDEF">
+
+
 <?php
 /* Embedding account credentials isn't ideal...preferable to
  * store in a separate file that is included by PHP (and not
@@ -26,13 +48,40 @@
 			
 	//TODO: SQL INJECTION PROTECTION
 	$result = $link->query($sql);
+	
+	echo "<table border='1'>
+	<tr>
+		<th>Squadron</th>
+		<th>Cadet Name</th>
+		<th>Class Year</th>
+		<th>Total Form 10s</th>
+		<th>Positive Form 10s</th>
+	</tr>";
 
 	if ($result->num_rows > 0){
 		while($row = $result->fetch_assoc() ){
-			echo $row["cadetName"]."  ".$row["squad"]."  ".$row["classYear"]."   ".$row["total"]."   ".$row["pos"]."<br>";
+			echo "<tr>";
+			echo "<td>" . $row['squad'] . "</td>";
+			echo "<td>" . $row['cadetName'] . "</td>";
+			echo "<td>" . $row['classYear'] . "</td>";
+			echo "<td>" . $row['total'] . "</td>";
+			echo "<td>" . $row['pos'] . "</td>";
+			echo "</tr>";
+			
+
 		}
 	} 
 	else {
 		echo "0 records";
 	}
 ?>
+			<!--echo $row["squad"]."  ".
+			     $row["cadetName"]."   ".
+				 $row["classYear"]."   ".
+				 $row["total"]."   ".
+				 $row["pos"].
+				 "<br>";-->
+
+
+</body>
+</html>
